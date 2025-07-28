b03897c0deb17ff14c6b0137a943b4c52149914f
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-07-28 13:23:48 +0800

    feat: add dynamic  sidebar generate from routes meta

--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -1,7 +1,27 @@
-export function Sidebar(){
+import router, { RoutesWithMeta } from '../../router'
+import { NavLink } from 'react-router-dom'
+import './Sidebar.css'
+
+export function Sidebar() {
+    const secondryRoutes = router.routes[0].children
+    if (!secondryRoutes) return null
+
+    const renderSidebar = (routes: RoutesWithMeta) => {
+        return routes.map((route) => (
+            <div key={route.id || route.path}>
+                <NavLink to={route.path || '/'}>{route.meta.title}</NavLink>
+                {route.children && (
+                    <div className="sidebar-children">
+                        {renderSidebar(route.children as RoutesWithMeta)}
+                    </div>
+                )}
+            </div>
+        ))
+    }
+
     return (
-        <>
-            Sidebar
-        </>
+        <div className="sidebar">
+            {renderSidebar(secondryRoutes as RoutesWithMeta)}
+        </div>
     )
 }
