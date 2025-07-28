631bf034312a325bd0ae792b7ab3c32c6e9357f9
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-07-28 14:08:54 +0800

    feat: add basic layout and header

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,21 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './header/Header'
 import { Sidebar } from './sidebar/Sidebar'
+import './Layout.css'
 
 export function Layout() {
     return (
         <div className="layout">
-            <Header />
-            <Sidebar />
-            {/* 二级路由 */}
-            <main className="content">
-                <Outlet />
-            </main>
+            <div className="layout-left">
+                <Sidebar />
+            </div>
+            <div className="layout-right">
+                <Header />
+                {/* 二级路由 */}
+                <main className="layout-content">
+                    <Outlet />
+                </main>
+            </div>
         </div>
     )
 }
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,8 +1,21 @@
-export function Header(){
-    return (
-        <>
-            Header
-            
-        </>
-    )
+import './Header.css'
+import { useRouteLoaderData } from 'react-router-dom'
+import router, { RouteWithMeta } from '../../router'
+
+export function Header() {
+    const getCurrentRoute = () => {
+        const sencondryRoutes = router.routes[0].children
+        if (!sencondryRoutes) return
+        return sencondryRoutes.find(
+            (route) => !!useRouteLoaderData(route.id as string)
+        )
+    }
+
+    const getCurrentTitle = () => {
+        const currentRoute = getCurrentRoute()
+        if (!currentRoute) return null
+        return (currentRoute as RouteWithMeta).meta.title
+    }
+
+    return <div className="header">{getCurrentTitle()}</div>
 }
