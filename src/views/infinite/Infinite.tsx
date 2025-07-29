a121aec74b240d2e6ff81185cf3cd883d5c061a1
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-07-30 00:12:01 +0800

    feat: add throttle util function, use debounce as hack to simulate 'throttle'

--- src/views/infinite/Infinite.tsx
+++ src/views/infinite/Infinite.tsx
@@ -1,36 +1,44 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import { useImmer } from 'use-immer'
-import { throttle } from '../../utils'
+import { debounce } from '../../utils'
+
+/**
+ * IntersectionObserver 其实只在出现与不出现切换的时候才触发
+ * 这里一直在视口内却依旧触发了，是组件重新渲染导致重建了 observer
+ * 节流误用为防抖时，useCallback 使 addList 一直是同一个函数，计数器是正常工作的，导致 addList 一直无法触发，于是视图不再刷新。此时隔开防抖的时间在 vscode 按保存，导致组件刷新，于是 addList 就触发了，所以按一下保存才会刷新一次。而没有 useCallback 包起来时，一直创建新的实例，就导致防抖失效了，所以可以一直触发 addList，导致无限滚动的假象
+ * 不对，我弄反了，这里应该是节流的。写成防抖了，防抖因为延迟触发 addList 倒是可行，节流没赶上监控所以没触发……两次监控之间的间隔在 10 ~ 20ms，笑死了
+ * 那还是将错就错，造成无限滚动的假象吧
+ * 不过，在初始就有滚动条的情况下， throttle 才是正解，用户抽风那他会再刷一次的，不用操心
+ */
 
 export function Infinite() {
     const [list, setList] = useImmer([0])
+    const guardRef = useRef<HTMLSpanElement| null>(null)
 
-    const addList = throttle(() => {
+    const addList = useCallback(debounce(() => {
         setList((draft) => {
             draft.push(Math.floor(Math.random() * 10))
         })
-    }, 500)
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                console.log('inspect')
-                addList()
-            }
-        })
-    }, {
-        threshold: 1
-    })
+    }, 500),[])
 
     useEffect(() => {
-        const guard = document.getElementById('guard')
-        if (!guard) return
-        observer.observe(guard)
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    addList()
+                }
+            })
+        }, {
+            threshold: 1
+        })
+        
+        if (guardRef.current) {
+            observer.observe(guardRef.current)
+        }
         return () => {
             observer.disconnect()
         }
-    })
-    // if (!guard) return null
+    },[list])
 
     return (
         <>
@@ -41,7 +49,7 @@ export function Infinite() {
                         <li key={index}>{item}</li>
                     ))}
                 </ul>
-                <span id="guard" style={{padding: '10px', backgroundColor: '#eee'}}>Loading...</span>
+                <span id="guard" ref={guardRef} style={{padding: '10px', backgroundColor: '#eee'}}>Loading...</span>
             </div>
         </>
     )
